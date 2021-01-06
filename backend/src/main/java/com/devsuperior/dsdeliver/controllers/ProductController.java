1e088622b1f6b9b93a1f2cfd19f4b11527f993ea
Raífe <raife.tec@gmail.com>
2021-01-06 03:40:00 -0300

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/ProductController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsdeliver.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsdeliver.dto.ProductDto;
+import com.devsuperior.dsdeliver.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+	
+	@Autowired
+	private ProductService service;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<ProductDto>> findAll(){
+		List<ProductDto> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+}
---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.ProductDto;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<ProductDto> findAll(){
+		List<Product> list = repository.findAllByOrderByNameAsc();
+		return list.stream().map(x -> new ProductDto(x)).collect(Collectors.toList());
+	}
+	
+
+}
